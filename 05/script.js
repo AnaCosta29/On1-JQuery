4752b1710cac2a2883eab8f88c85b84a547a7c87
AnaCosta <luizacosta64@hotmail.com>
2019-04-19 16:07:03 -0300

    ParaCasaDone

--- 05/script.js
+++ 05/script.js
@@ -1,13 +1,25 @@
 $(function(){
 
-    $('p')
-    .mouseup(function(){
-        $(this).append("<span style='color:#f00;'> Subiuuuuu </span>");
-    })
-    .mousedown(function(){
-        $(this).append("<span style='color:#00f;'> Desceeeuuuu </span>");
-    });
+    $('#carousel img:eq(0)').addClass('ativo').show();
+    let legenda = $('.ativo').attr('alt');
+    $('#carousel').prepend("<p>"+legenda+"</p>");
+    setInterval(slider,2500);
 
+    function slider(){
 
+        if($('.ativo').next().length){
+
+            $('.ativo').fadeOut().removeClass('ativo').next().fadeIn().addClass('ativo');
+
+        }else{
+            $('.ativo').fadeOut().removeClass('ativo');
+            $('#carousel img:eq(0)').fadeIn().addClass('ativo');
+
+        }
+
+        let legenda = $('.ativo').attr('alt');
+        $('#carousel p').hide().html(legenda).delay(500).fadeIn();
+
+    }
 });
 
